06a43f3feba2acf92eb6c07933fef3b95fdbf7c4
jhonnatan suarez <jonathan.sm87@gmail.com>
2026-05-19 14:22:34 -0500

    refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 import { join } from 'path';
-import { EnvConfiguration } from './config/env.config';
-import { JoiValidationSchema } from './config/joi.validation';
+import { EnvConfiguration } from './config/configuration';
+import { JoiValidationSchema } from './config/validation.schema';
 import { CommonModule } from './common/common.module';
-import { CustomerModule } from './customer/customer.module';
+import { CustomerModule } from './modules/customer/customer.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -21,7 +22,8 @@ import { CustomerModule } from './customer/customer.module';
       dbName: 'plantilla'
     }),
     CommonModule,
-    CustomerModule
+    CustomerModule,
+    AuthModule
   
   ],
 })
